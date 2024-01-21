f39d9075cee8d6dc5bd3a8122082417d312447e1
MarekVVK <marek.stankovskij@vvk.lt>
2024-01-21 22:44:42 +0200

    V 1.2
    Added:
    -Employee dashboard
    -All conferences list in Employee dashboard
    -Future conferences list in Client dashboard
    -Register/Unregister option for Clients
    -View Registered Clients for Employee

--- app/Http/Controllers/ConferenceRegistrationController.php
+++ app/Http/Controllers/ConferenceRegistrationController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Conference;
 use App\Models\User;
-use Auth;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 
 class ConferenceRegistrationController extends Controller
